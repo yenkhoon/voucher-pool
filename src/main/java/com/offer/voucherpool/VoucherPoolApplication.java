74d216078cb4d723697ea6b13434eb00d1f060e2
y k <tankhoon@gmail.com>
2020-11-23 04:28:20 +0800

    initial commit

---
+++ src/main/java/com/offer/voucherpool/VoucherPoolApplication.java
@@ -0,0 +1,13 @@
+package com.offer.voucherpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offer/voucherpool/VoucherPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offer.voucherpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
